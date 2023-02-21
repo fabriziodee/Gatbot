ec7acceb892b0d177cab2051971ef762792c025a
fabriziodee <124698914+fabriziodee@users.noreply.github.com>
2023-02-22 00:59:42 +0100

    Update config.js

--- config.js
+++ config.js
@@ -30,7 +30,7 @@ global.prems = ['393518398856'],
 //  en = English         pt = Português 
 //  ar = عرب  
 
-global.lenguajeGB = es  //Idioma de GataBot, Ejemplo: es | en | pt...
+global.lenguajeGB = en  //Idioma de GataBot, Ejemplo: es | en | pt...
 
 //━━━━━━━━━━━━━━━━━━━━ ฅ^•ﻌ•^ฅ
 
